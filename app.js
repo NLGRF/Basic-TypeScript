b3f842799a032c76038973643db951c0d9700c8e
NLGRF <nongrace6654@gmail.com>
2017-09-17 20:08:53 +0700

    Variable

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
+var names = "Hello TypeScript";
+var count = 100;
+var str = true;
+var isRunning = false;
+var fname = true;
 console.log("HelloWorld");
+document.getElementById("id1").innerHTML = fname;
+document.write("Count is Type :" + typeof (count) + "<br>");
+document.write("Str is Type :" + typeof (str) + "<br>");
--- app.ts
+++ app.ts
@@ -1,1 +1,10 @@
+let names:string="Hello TypeScript";
+let count:number=100;
+let str:boolean=true;
+let isRunning=false;
+let fname:any=true;
 console.log("HelloWorld");
+
+document.getElementById("id1").innerHTML=fname;
+document.write("Count is Type :"+typeof(count)+"<br>");
+document.write("Str is Type :"+typeof(str)+"<br>");
