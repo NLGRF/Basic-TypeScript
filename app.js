04244c7c4da01a3f29cbdafe63c94aef207bcd90
NLGRF <nongrace6654@gmail.com>
2017-09-18 14:30:32 +0700

    Constructor & Method

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 var Employee = /** @class */ (function () {
-    function Employee() {
-        console.log("Create object");
+    function Employee(id, names, sex, salary) {
+        this.id = id;
+        this.names = names;
+        this.sex = sex;
+        this.salary = salary;
     }
+    Employee.prototype.display = function () {
+        console.log("ID = " + this.id);
+        console.log("NAME = " + this.names);
+        console.log("SEX = " + this.sex);
+        console.log("SALARY = " + this.salary);
+    };
     return Employee;
 }());
-var emp1 = new Employee();
+var emp1 = new Employee(101, "Non", 1, 500);
+emp1.display();
+var emp2 = new Employee(100, "Grace", 0, 99);
+emp2.display();
--- app.ts
+++ app.ts
@@ -1,11 +1,25 @@
 class Employee{
   private id:number;
   private names:string;
+  // Male = 1
+  // Female = 0
   private sex:number;
   private salary:number;
 
-  constructor(){
-    console.log("Create object");
+  constructor(id:number,names:string,sex:number,salary:number){
+    this.id=id;
+    this.names=names;
+    this.sex=sex;
+    this.salary=salary;
+  }
+  display():void{
+    console.log("ID = "+this.id);
+    console.log("NAME = "+this.names);
+    console.log("SEX = "+this.sex);
+    console.log("SALARY = "+this.salary);
   }
 }
-let emp1=new Employee();
+let emp1=new Employee(101,"Non",1,500);
+emp1.display();
+let emp2=new Employee(100,"Grace",0,99);
+emp2.display();
