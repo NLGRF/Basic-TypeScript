9a866519ee4bf25f10ee9cc6b4e2f7d1bfd697e2
NLGRF <nongrace6654@gmail.com>
2017-09-18 13:51:56 +0700

    Class Object OOP Programming

--- app.js
+++ app.js
@@ -1,30 +1,7 @@
-function showMessage() {
-    console.log("TypeScript Tutorial");
-}
-// Optional Parameter
-function addProduct(id, name, price) {
-    console.log("ID :" + id);
-    console.log("NAMES :" + name);
-    if (price != undefined) {
-        console.log("PRICE :" + price);
+var Employee = /** @class */ (function () {
+    function Employee() {
+        console.log("Create object");
     }
-}
-// Rest Parameter
-function addNumber() {
-    var nums = [];
-    for (var _i = 0; _i < arguments.length; _i++) {
-        nums[_i] = arguments[_i];
-    }
-    var sum = 0;
-    var i;
-    for (i = 0; i < nums.length; i++) {
-        sum += nums[i];
-    }
-    console.log("Summation :" + sum);
-}
-addNumber(1, 2);
-addNumber(1, 2, 3);
-addNumber(1, 2, 3, 4);
-addNumber(1, 2, 3, 4, 5);
-addProduct(100, "Iphone");
-addProduct(101, "Iphone", 15000);
+    return Employee;
+}());
+var emp1 = new Employee();
--- app.ts
+++ app.ts
@@ -1,27 +1,11 @@
-function showMessage(){
-  console.log("TypeScript Tutorial");
-}
-// Optional Parameter
-function addProduct(id:number,name?:string,price?:number) {
-    console.log("ID :"+id);
-    console.log("NAMES :"+name);
-    if(price!=undefined){
-    console.log("PRICE :"+price);
-    }
-}
-// Rest Parameter
-function addNumber(...nums:number[]){
-  var sum:number=0;
-  var i;
-  for(i=0;i<nums.length;i++){
-    sum+=nums[i];
-  }
-  console.log("Summation :"+sum);
-}
-addNumber(1,2);
-addNumber(1,2,3);
-addNumber(1,2,3,4);
-addNumber(1,2,3,4,5);
+class Employee{
+  private id:number;
+  private names:string;
+  private sex:number;
+  private salary:number;
 
-addProduct(100,"Iphone");
-addProduct(101,"Iphone",15000);
+  constructor(){
+    console.log("Create object");
+  }
+}
+let emp1=new Employee();
