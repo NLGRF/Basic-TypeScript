e510a34b82560f70ad039f7dc0e6a81682624dae
NLGRF <nongrace6654@gmail.com>
2017-09-18 13:42:14 +0700

    Rest Parameter Function

--- app.js
+++ app.js
@@ -9,5 +9,22 @@ function addProduct(id, name, price) {
         console.log("PRICE :" + price);
     }
 }
+// Rest Parameter
+function addNumber() {
+    var nums = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        nums[_i] = arguments[_i];
+    }
+    var sum = 0;
+    var i;
+    for (i = 0; i < nums.length; i++) {
+        sum += nums[i];
+    }
+    console.log("Summation :" + sum);
+}
+addNumber(1, 2);
+addNumber(1, 2, 3);
+addNumber(1, 2, 3, 4);
+addNumber(1, 2, 3, 4, 5);
 addProduct(100, "Iphone");
 addProduct(101, "Iphone", 15000);
--- app.ts
+++ app.ts
@@ -9,5 +9,19 @@ function addProduct(id:number,name?:string,price?:number) {
     console.log("PRICE :"+price);
     }
 }
+// Rest Parameter
+function addNumber(...nums:number[]){
+  var sum:number=0;
+  var i;
+  for(i=0;i<nums.length;i++){
+    sum+=nums[i];
+  }
+  console.log("Summation :"+sum);
+}
+addNumber(1,2);
+addNumber(1,2,3);
+addNumber(1,2,3,4);
+addNumber(1,2,3,4,5);
+
 addProduct(100,"Iphone");
 addProduct(101,"Iphone",15000);
